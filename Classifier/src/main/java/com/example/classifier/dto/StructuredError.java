64f28ce2045a4feb7309f0ac830707aeb7f7ede3
Grishagrishaa <59513494+Grishagrishaa@users.noreply.github.com>
2022-07-13 01:03:48 +0300

    <->

---
+++ Classifier/src/main/java/com/example/classifier/dto/StructuredError.java
@@ -0,0 +1,21 @@
+package com.example.classifier.dto;
+
+import java.util.List;
+
+public class StructuredError {
+    private final String logref;
+    private final List<ErrorMessage>  errorMessagesList;
+
+    public StructuredError(List<ErrorMessage> errorMessagesList) {
+        this.logref = "structured_error";
+        this.errorMessagesList = errorMessagesList;
+    }
+
+    public String getLogref() {
+        return logref;
+    }
+
+    public List<ErrorMessage> getErrorMessagesList() {
+        return errorMessagesList;
+    }
+}
